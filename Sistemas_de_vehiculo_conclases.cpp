ee278b63d280d7d64ea4dbdc376f3f3acf3b4778
Tu Nombre <tuemail@example.com>
2026-03-13 00:24:06 -0600

    ready

--- Sistemas_de_vehiculo_conclases.cpp
+++ Sistemas_de_vehiculo_conclases.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Vehiculo{
 private:
     string pidevias;
-    int avanzar=0;
+    string avanzar;
 public:
 
     string estado;
@@ -15,9 +15,20 @@ public:
         
     }
 
-    void setavanzar(int a){
+    void modificarpidevias(string pp){
+
+        pidevias=pp;
+
+    }
+
+    void setavanzar(string a){
 
         avanzar=a;
+    }
+
+    void modificaravanzar(string aa){
+
+        avanzar=aa;
     }
 
     void setestado(string s){
@@ -25,12 +36,18 @@ public:
         estado=s;
     }
 
+    void modificarestado(string ss){
+
+        estado=ss;
+
+    }
+
     string getpidevias(){
 
         return pidevias;
     }
 
-    int getavanzar(){
+    string getavanzar(){
 
         return avanzar;
     }
@@ -41,20 +58,20 @@ public:
     }
 
 
-    void encendiendo(){
+    void encendiendopagado(){
 
-        cout<<"Vehiculo Encendido \n"<<estado<<"\n";
+        cout<<"Vehiculo "<<estado<<"\n";
     }
 
-    void avanzando(){
+    void avanzandoretrocediento (){
 
-        cout<<"El vehiculo esta avanzando!!"<<avanzar<<"\n";
+        cout<<"El vehiculo esta "<<avanzar<<"\n";
 
     }
 
-    void izquierda(){
+    void derechaizquierda (){
 
-        cout<<"El Vehiculo esta girando a la izquierda"<<pidevias<<"\\n";
+        cout<<"El Vehiculo esta girando a la "<<pidevias<<"\\n";
     }
 };
 
@@ -64,13 +81,73 @@ int opcion;
 
     do
     {
-        /* code */
-    } while (condition);
+        cout<<"------Bienvenido a tu Automovil------\n";
+        cout<<" Preciona la opcion que deseas ejecutar\n";
+        cout<<"1. Encender el vehiculo.\n";
+        cout<<"2. Apagar el Vehiculo.\n";
+        cout<<"3. Avanzar.\n";
+        cout<<"4. Retroceder.\n";
+        cout<<"5. Girar hacia la derecha >>>>>\n";
+        cout<<"6. Girar hacia la izquierda <<<<<\n";
+        cin>>opcion;
+
+        switch (opcion)
+        {
+        case 1:{
+            Vehiculo encender;
+            encender.setestado("Encendido -----0-----\n");
+            cout<<encender.getestado()<<endl;
+            encender.encendiendopagado();
+
+            break;
+        }
+            case 2:{
+                
+            Vehiculo apagado;
+            apagado.modificarestado("Apagado");
+            cout<<apagado.getestado()<<endl;
+            apagado.encendiendopagado();
+
+            break;
+        }
+                case 3:{
+
+                Vehiculo avanzar;
+                avanzar.setavanzar("Avanznado =====10km=====\n");
+                cout<<avanzar.getavanzar()<<endl;
+                avanzar.avanzandoretrocediento();
+
+            break;
+        }            
+                    case 4:{
+
+                    Vehiculo retroceder;
+                    retroceder.setavanzar("Retrocediendo =====-10km=====\n");
+                    cout<<retroceder.getavanzar()<<endl;
+                    retroceder.avanzandoretrocediento();
+
+            break;
+        }
+                    case 5:{
+
+                    Vehiculo izquierda;
+                    izquierda.setpidevias("<<<<<<<<<<\n");
+                    cout<<izquierda.getpidevias()<<endl;
+                    izquierda.derechaizquierda();
+            break;
+        }                
+
+        default:
+
+                    Vehiculo derecha;
+                    derecha.setpidevias(">>>>>>>>>>\n");
+                    cout<<derecha.getpidevias()<<endl;
+                    derecha.derechaizquierda();
+
+            break;
+        }
+        
+    } while (opcion!=6);
     
-
-
-
-
-
     return 0;
 }
